180160c290c558788288818b6573098530276b5a
AGUSTIN ZAID ALESSI <48385003@ort>
2024-08-01 13:47:24 -0300

    Creacion Proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MiWebPersonal_Alessi.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
